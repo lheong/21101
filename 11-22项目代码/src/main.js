af1ac640b288f65e3c3218d5af9f15787c11998e
lheong <296623133@qq.com>
2022-11-22 23:51:53 +0800

    项目

--- 11-22项目代码/src/main.js
+++ 11-22项目代码/src/main.js
@@ -7,6 +7,15 @@ import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
+import VueQuillEditor from 'vue-quill-editor'
+
+// 引入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.use(VueQuillEditor, /* { 默认全局 } */)
+
 new Vue({
   router,
   store,
--- 11-22项目代码/src/views/visitor.vue
+++ 11-22项目代码/src/views/visitor.vue
@@ -1,22 +1,64 @@
 <template>
-<div>
-    用户分布
-</div>
+  <div>
+    <div>
+      <div id="main" :style="{ width: '500px', height: '500px' }"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import * as echarts from "echarts";
 export default {
-  name: '',
+  name: "",
   components: {},
-  data () {
-    return {
-    }
+  data() {
+    return {};
   },
-  created () { },
-  mounted () { },
-  methods: {}
-}
-</script>
-<style scoped lang='scss'>
+  created() {},
+  mounted() {
+    var chartDom = document.getElementById("main");
+    var myChart = echarts.init(chartDom);
+    var option;
 
-</style>
+    option = {
+      title: {
+        text: "Referer of a Website",
+        subtext: "Fake Data",
+        left: "center",
+      },
+      tooltip: {
+        trigger: "item",
+      },
+      legend: {
+        orient: "vertical",
+        left: "left",
+      },
+      series: [
+        {
+          name: "Access From",
+          type: "pie",
+          radius: "50%",
+          data: [
+            { value: 1048, name: "Search Engine" },
+            { value: 735, name: "Direct" },
+            { value: 580, name: "Email" },
+            { value: 484, name: "Union Ads" },
+            { value: 300, name: "Video Ads" },
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)",
+            },
+          },
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  },
+  methods: {},
+};
+</script>
+<style scoped lang="scss"></style>
