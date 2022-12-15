c8b27db67b3513a2c07e23c085b7d08647f274fe
lheong <296623133@qq.com>
2022-12-15 16:03:14 +0800

    12-14

---
+++ 12-14/src/views/reports/ReportsView.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div id="main" style="height: 400px; width: 100%"></div>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts"
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {
+    var chartDom = document.getElementById("main")
+    var myChart = echarts.init(chartDom)
+    var option
+
+    option = {
+      title: {
+        text: "后台管理"
+      },
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "cross",
+          label: {
+            backgroundColor: "#6a7985"
+          }
+        }
+      },
+      legend: {
+        data: ["华东", "华南", "华北", "西部", "其他"]
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: "category",
+          boundaryGap: false,
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        }
+      ],
+      yAxis: [
+        {
+          type: "value"
+        }
+      ],
+      series: [
+        {
+          name: "华东",
+          type: "line",
+          stack: "Total",
+          areaStyle: {},
+          emphasis: {
+            focus: "series"
+          },
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: "华南",
+          type: "line",
+          stack: "Total",
+          areaStyle: {},
+          emphasis: {
+            focus: "series"
+          },
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: "华北",
+          type: "line",
+          stack: "Total",
+          areaStyle: {},
+          emphasis: {
+            focus: "series"
+          },
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: "西部",
+          type: "line",
+          stack: "Total",
+          areaStyle: {},
+          emphasis: {
+            focus: "series"
+          },
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: "其他",
+          type: "line",
+          stack: "Total",
+          label: {
+            show: true,
+            position: "top"
+          },
+          areaStyle: {},
+          emphasis: {
+            focus: "series"
+          },
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    }
+
+    option && myChart.setOption(option)
+  },
+  methods: {}
+}
+</script>
+<style scoped lang="scss"></style>
