053b58fdc6a6483ce98457f030b591df42c15825
lheong <296623133@qq.com>
2023-01-11 23:46:14 +0800

    1-11

---
+++ 1-11/src/views/jurisdiction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">社保</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {}
+  },
+  methods: {},
+  created() {}
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 1-11/src/views/socialSecurity.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">审批</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {}
+  },
+  methods: {},
+  created() {}
+}
+</script>
+
+<style lang="scss" scoped></style>
